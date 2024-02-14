0058088088555b56d6d056e5417fe7363a69a5fd
JoysgitHub <114746470+JoysgitHub@users.noreply.github.com>
2024-02-14 23:22:14 +0000

    LOGBOOK location change

--- LOGBOOK/wk1ex1.php
+++
@@ -1,4 +0,0 @@
-/* This code returns the various details about the backend including
- * the php version, Operating system, file location, webserver etc. */
-
-<?php phpinfo();?>
--- LOGBOOK/wk1ex2.php
+++
@@ -1,6 +0,0 @@
-/* Returns the user agent used by the browser */ 
-/* this includes the OS, Browser versions. */
-<?php echo $_SERVER["HTTP_USER_AGENT"];?> */
-
-
-
